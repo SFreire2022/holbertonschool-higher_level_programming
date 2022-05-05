b7e01293bd76f648eabc2ea1c9621bbd56a5c0aa
SFreire2022 <sebastiancazenave@gmail.com>
2022-05-04 22:33:40 -0300

    Fix task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-def mutiply_list_map(my_list=[], number=0):
-    return list(map(lambda n: n * number, my_list))
+def best_score(a_dictionary):
+    if a_dictionary:
+        return max(a_dictionary, key=a_dictionary.get)
+    return None
