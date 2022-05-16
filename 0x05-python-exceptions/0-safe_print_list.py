68a8d47d3735497abe2f80e925635e1b900b4692
SFreire2022 <sebastiancazenave@gmail.com>
2022-05-16 20:52:48 -0300

    Chanage format for print function and except condition declaration

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,8 +4,8 @@ def safe_print_list(my_list=[], x=0):
         return (0)
     try:
         for i in range(x):
-            print(my_list[i], end="")
-    except:
+            print(f'{my_list[i]}', end="")
+    except IndexError:
         # if x is bigger than list elements decrease one
         i = i - 1
     finally:
