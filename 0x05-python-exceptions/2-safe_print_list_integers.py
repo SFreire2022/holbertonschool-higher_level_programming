2c756ca5ee0c1077702b15153071aa49b3dba743
SFreire2022 <sebastiancazenave@gmail.com>
2022-05-16 22:06:22 -0300

    Add generic Exception

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,7 +8,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             num = num + 1
-        except (ValueError, TypeError, IndexError):
+        except (ValueError, TypeError, IndexError, Exception):
             pass
     print()
     return(num)
