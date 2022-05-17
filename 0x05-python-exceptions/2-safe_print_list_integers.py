fdea55bc6c7914ec1498aa42d7bdc165e5f7a927
SFreire2022 <sebastiancazenave@gmail.com>
2022-05-16 22:10:58 -0300

    change pass for continue

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,12 +4,11 @@ def safe_print_list_integers(my_list=[], x=0):
         print()
         return (0)
     num = 0
-    i = 0
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
             num = num + 1
         except (ValueError, TypeError, IndexError, Exception):
-            pass
+            continue
     print()
     return(num)
