fb41bd29f1c7ed7259261fa300458dd7d4ea8d1f
SFreire2022 <sebastiancazenave@gmail.com>
2022-05-16 22:18:37 -0300

    change exception

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,7 +7,7 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
-        except (ValueError, TypeError):
+        except Exception:
             continue
         else:
             num = num + 1
