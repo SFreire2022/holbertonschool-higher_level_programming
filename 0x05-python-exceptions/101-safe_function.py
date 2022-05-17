efcb7942b6eb2c1f7e508d0f02efbdd3378c043c
SFreire2022 <sebastiancazenave@gmail.com>
2022-05-16 23:04:58 -0300

    Add task 101

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_function(fct, *args):
+    import sys
+    try:
+        res = fct(*args)
+    except Exception as error:
+        print("Exception:", error, file=sys.stderr)
+        res = None
+    finally:
+        return (res)
