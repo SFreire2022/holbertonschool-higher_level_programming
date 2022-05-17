3077239857d5f992a4a3384486421e62e8d594a1
SFreire2022 <sebastiancazenave@gmail.com>
2022-05-16 22:21:18 -0300

    Leave index error whitout handling

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,7 +7,7 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
-        except Exception:
+        except (ValueError, TypeError):
             continue
         else:
             num = num + 1
