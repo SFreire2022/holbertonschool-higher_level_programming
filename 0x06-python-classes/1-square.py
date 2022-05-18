b1f888e9a241bceb9fa221e485fe265c95d03f66
SFreire2022 <sebastiancazenave@gmail.com>
2022-05-17 21:51:52 -0300

    Add task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module Square"""
+
+
+class Square:
+    """Definition for class Square adding first atribute size"""
+    def __init__(self, size):
+        """Private instance attribute: size"""
+        self.__size = size
