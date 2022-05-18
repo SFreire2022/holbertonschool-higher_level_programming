5454d8a9e6df289d8db6c69014afd6819bc9cf63
SFreire2022 <sebastiancazenave@gmail.com>
2022-05-17 22:08:16 -0300

    Change indentation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,8 +6,8 @@ class Square:
     """Definition for class Square adding first atribute size and def value"""
     def __init__(self, size=0):
         """Private instance attribute: size
-            -Check int data type, if not rise TypeError exception
-			-Check if value is positive, if not rise ValueError exception"""
+        -Check int data type, if not rise TypeError exception
+        -Check if value is positive, if not rise ValueError exception"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
