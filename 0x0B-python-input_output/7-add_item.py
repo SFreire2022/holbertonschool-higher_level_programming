3b8ea6ed46978d702fcb8590a9c0513be9072a59
SFreire2022 <sebastiancazenave@gmail.com>
2022-06-01 00:00:20 -0300

    add task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Script that adds all arguments to a Python list object,
+and save them to a JSON file"""
+from sys import argv
+
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+
+try:
+    myfile = open(filename, 'r')
+    myobj = load_from_json_file(filename)
+except Exception:
+    myobj = []
+
+for arg in argv[1:]:
+    myobj.append(arg)
+
+save_to_json_file(myobj, "add_item.json")
