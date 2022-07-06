484733d496035b0122068b53b511d5812488a4d4
SFreire2022 <sebastiancazenave@gmail.com>
2022-07-05 21:24:41 -0300

    Add task 5

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- Show full description for the table first_table from the database hbtn_0c_0
+-- the dbname will be passed in the connection sentence
+-- As of MySQL 8.0.16, MySQL implements CHECK constraints and SHOW CREATE TABLE displays them. All CHECK constraints are displayed as table constraints.
+SHOW CREATE TABLE first_table;
