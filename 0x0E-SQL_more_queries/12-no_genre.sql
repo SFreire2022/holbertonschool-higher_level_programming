482ca2d9e2bd70e6f3151beb4a889dadcda3e53e
SFreire2022 <sebastiancazenave@gmail.com>
2022-07-06 23:08:13 -0300

    Fix join type

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -4,6 +4,6 @@
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
-JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.genre_id is NULL
 ORDER BY tv_shows.title;
