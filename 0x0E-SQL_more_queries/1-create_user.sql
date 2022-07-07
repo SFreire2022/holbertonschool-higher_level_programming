ba657c208bb255374efeedcc16b188cccb8f8601
SFreire2022 <sebastiancazenave@gmail.com>
2022-07-06 21:49:31 -0300

    Fix sintax if not exists

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,2 @@
--- Script that creates the MySQL server user user_0d_1 if not exist and grant all privileges.
-CREATE USER IF NOT EXIST 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- Script that creates the MySQL server user user_0d_1 if not exists.
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
