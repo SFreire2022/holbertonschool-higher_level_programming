6a3238573130a8faaf0757242dcf1cacfb8af1e4
SFreire2022 <sebastiancazenave@gmail.com>
2022-07-06 21:46:42 -0300

    Add grant all access to all

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,3 @@
 -- Script that creates the MySQL server user user_0d_1 if not exist and grant all privileges.
 CREATE USER IF NOT EXIST 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
