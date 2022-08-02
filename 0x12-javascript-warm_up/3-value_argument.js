ce9aefcc84879ed0a84b57ebcbe7b20faebc0176
SFreire2022 <sebastiancazenave@gmail.com>
2022-08-01 21:20:54 -0300

    Don t prit all args pased just first

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,8 +3,9 @@ if (process.argv[2] === undefined) {
   console.log('No argument');
 } else {
   // print all the arguments pased to process.argv[1]
-  const args = process.argv.slice(2);
-  args.forEach((arg) => {
-    console.log(arg);
-  });
+  // const args = process.argv.slice(2);
+  // args.forEach((arg) => {
+  //   console.log(arg);
+  // });
+  console.log(process.argv[2]);
 }
