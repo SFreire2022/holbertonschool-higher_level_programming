46aa4a15c5c28666021077c9aa02241e47e77b7c
SFreire2022 <sebastiancazenave@gmail.com>
2022-08-01 23:50:22 -0300

    Add task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+let side = parseInt(process.argv[2]);
+const i = side;
+if (Number.isNaN(side)) {
+  console.log('Missing size');
+} else {
+  for (; side > 0; side--) {
+    console.log('x'.repeat(i));
+  }
+}
