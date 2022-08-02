3e74ff7abc16908ae3d79ceb98a25c72b2638cc2
SFreire2022 <sebastiancazenave@gmail.com>
2022-08-01 21:17:38 -0300

    Add task 3

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+if (process.argv[2] === undefined) {
+  console.log('No argument');
+} else {
+  // print all the arguments pased to process.argv[1]
+  const args = process.argv.slice(2);
+  args.forEach((arg) => {
+    console.log(arg);
+  });
+}
