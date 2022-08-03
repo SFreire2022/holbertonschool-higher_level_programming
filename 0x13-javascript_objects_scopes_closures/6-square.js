ecd24712066415969bec1f8cc42e31116160b485
SFreire2022 <sebastiancazenave@gmail.com>
2022-08-02 22:57:52 -0300

    Adapt c undefine test to meet requirements

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,7 +3,10 @@ const OldSquare = require('./5-square');
 
 module.exports = class Square extends OldSquare {
   charPrint (c) {
-    c = !c ? 'X' : c;
+    if (typeof (c) === 'undefined') {
+      c = 'X';
+    }
+    // c = !c ? 'X' : c; requirment no meet but works fine
     let i = this.height;
     for (; i > 0; i--) {
       console.log(c.repeat(this.width));
