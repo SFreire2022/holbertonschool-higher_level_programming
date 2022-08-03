9c8d451f4164ea625edcb4b19ae2fea7a6494a95
SFreire2022 <sebastiancazenave@gmail.com>
2022-08-02 23:32:26 -0300

    Change var definition to let and const to meet requirements

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  var rev = [];
-  for (var i = list.length - 1; i >= 0; i--) {
+  const rev = [];
+  for (let i = list.length - 1; i >= 0; i--) {
     rev.push(list[i]);
   }
   return rev;
