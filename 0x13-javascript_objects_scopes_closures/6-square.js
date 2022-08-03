d57f30e1cf477457acb5ef4af66076a6ac56ac6f
SFreire2022 <sebastiancazenave@gmail.com>
2022-08-02 23:04:31 -0300

    Trying to understand what requirement does not meet

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const OldSquare = require('./5-square');
 
-module.exports = class Square extends OldSquare {
+class Square extends OldSquare {
   charPrint (c) {
     if (typeof (c) === 'undefined') {
       c = 'X';
@@ -12,4 +12,5 @@ module.exports = class Square extends OldSquare {
       console.log(c.repeat(this.width));
     }
   }
-};
+}
+module.exports = Square;
