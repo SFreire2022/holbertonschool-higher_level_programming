ad748573f51a70171b9d92ff6539acef515e692f
SFreire2022 <sebastiancazenave@gmail.com>
2022-08-07 18:02:20 -0300

    Add task 5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Python script that takes in an argument (state) and displays all cities
+in table of hbtn_0e_4_usa.
+Safe from MySQL injections!
+"""
+
+from sys import argv
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        charset="utf8")
+    state_arg = argv[4]
+    cursor = db.cursor()
+    sql_query = "SELECT cities.name FROM cities JOIN states\
+                ON cities.state_id = states.id\
+                WHERE states.name = %s ORDER by cities.id ASC"
+    cursor.execute(sql_query, (state_arg,))
+    res_query_rows = cursor.fetchall()
+
+    for row in res_query_rows:
+        if row[1] == state_arg:
+            print(row)
+    cursor.close()
+    db.close()
