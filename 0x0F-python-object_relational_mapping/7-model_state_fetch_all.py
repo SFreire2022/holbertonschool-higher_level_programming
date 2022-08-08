c0cf1854847f54128b47c6c489cf19d4f40f77b2
SFreire2022 <sebastiancazenave@gmail.com>
2022-08-08 12:05:03 -0300

    Test border case check 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
-    states = Session().query(State.id, State.name).order_by(State.id)
-    for id, name in states:
+    session = Session()
+    for id, name in session.query(State.id, State.name).order_by(State.id):
         print("{:d}: {}".format(id, name))
     Session().close()
