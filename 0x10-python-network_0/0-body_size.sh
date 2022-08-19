d8c8004e997d4240fba70633e629bbb8a0f14b83
SFreire2022 <sebastiancazenave@gmail.com>
2022-08-18 21:21:27 -0300

    Protect arg var 1

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Shell script thath return de content-length of the http response
-curl -sI $1 | grep "Content-Length" | awk '{print $2}'
+curl -sI "$1" | grep "Content-Length" | awk '{print $2}'
