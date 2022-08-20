fb82c433fe895dc5a42a8480c18bb3c4cf33ca50
SFreire2022 <sebastiancazenave@gmail.com>
2022-08-19 21:48:53 -0300

    Fix importing sys

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -2,6 +2,7 @@
 '''Script that takes in a URL, sends a request to the URL and displays
 the value of the variable X-Request-Id in the response header'''
 import requests
+import sys
 
 
 if __name__ == '__main__':
