a20673bef740537fc06bb3a38e5c3cae8e29c61f
SFreire2022 <sebastiancazenave@gmail.com>
2022-08-19 21:02:28 -0300

    Fix pycodestyle in line 16

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -13,4 +13,4 @@ if __name__ == '__main__':
     data = data.encode('utf-8')
     request = urllib.request.Request(sys.argv[1], data)
     with urllib.request.urlopen(req) as resp:
-        print(resp.read().decode('utf-8')
+        print(resp.read().decode('utf-8'))
