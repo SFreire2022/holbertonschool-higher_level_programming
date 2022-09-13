69c06f512e2e9a6e0cdb9f1329acf44fcfd855c2
SFreire2022 <sebastiancazenave@gmail.com>
2022-09-12 23:39:43 -0300

    Update using Axios

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
-const request = require('request');
+const axios = require('axios').default;
 const episode = process.argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/' + episode;
 
-request(url, function (err, resp, body) {
-  if (err) {
-    console.error('error:', err);
-  } else {
-    console.log(JSON.parse(body).title);
-  }
+axios.get(url).then(function (resp) {
+  console.log(resp.data.title);
 });
