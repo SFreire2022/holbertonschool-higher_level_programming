b1351d17bca0529f1b9aded4ce2a141f6e729670
SFreire2022 <sebastiancazenave@gmail.com>
2022-09-13 00:46:48 -0300

    Make use of new response

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -4,8 +4,8 @@ const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
 
 axios.get(url).then((resp) => {
   for (const character in resp.data.characters) {
-    axios.get(resp.data.characters[character]).then((resp) => {
-      console.log(resp.data.name);
+    axios.get(resp.data.characters[character]).then((resp2) => {
+      console.log(resp2.data.name);
     });
   }
 }).catch((error) => {
